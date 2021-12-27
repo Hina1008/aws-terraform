df259fe04a4e6ed43f737cf162885d3042b642c8
Hina1008 <shota.bamitonton@gmail.com>
2021-12-27 12:39:43 +0000

    VPCの設定

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ provider "aws" {
     profile = "mfa"
     region = "ap-northeast-1"
 }
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "handson"
+  }
+}
